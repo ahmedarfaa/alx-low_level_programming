ec943c2afaf8b0c944fa10c586d22062065e5297
ahmedarfaa <ahmedarfaa594@gmail.com>
2023-03-16 11:51:07 -0700

    101

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: C program that prints useing putchar
+ *
+ * Return: Always 0 (success)
+*/
+int main(void)
+{
+	write(STDOUT_FILENO, and that piece of art is useful"
+			\" - Dora Korpar, 2015-10-19\n, 59);
+	return (1);
+}
