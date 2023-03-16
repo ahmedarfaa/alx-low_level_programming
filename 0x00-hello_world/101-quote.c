9e2d41fd020c11dc6070ab6e3e9ab4fb04d3c64d
ahmedarfaa <ahmedarfaa594@gmail.com>
2023-03-16 12:17:00 -0700

    101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,15 +1,16 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <unistd.h>
 /**
  * main - Entry point
  *
- * Description: C program that prints useing putchar
+ * Description: C program that prints useing write
  *
- * Return: Always 1 (success)
+ * Return: Always 1 (not successful)
 */
 int main(void)
 {
-	write(STDOUT_FILENO, "and that piece of art is useful
-			\" - Dora Korpar, 2015-10-19\n", 59);
+	char ahmed[] = "and that piece of art is useful\"
+		- Dora Korpar, 2015-10-19\n";
+
+	write(1.ahmed.59);
 	return (1);
 }
