43cf8ea9119ee43e001124361d64e3f7189128c1
ahmedarfaa <ahmedarfaa594@gmail.com>
2023-03-17 10:44:03 -0700

    2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Main enrty point
+ *
+ * Description: A C program
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char alpha;
+
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+	putchar(alpha);
+	putchar(\n);
+
+	return (0);
+}
