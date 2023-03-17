0e9623b330792472dfb03df79d2e11b4f0f7a8c4
ahmedarfaa <ahmedarfaa594@gmail.com>
2023-03-17 11:02:02 -0700

    4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -9,10 +9,9 @@ int main(void)
 	char alpha;
 
 for (alpha = 'a'; alpha <= 'z'; alpha++)
-	if (alpha == e || alpha == q)
-	{	break;
+	if (alpha != e && alpha != q)
+	{	putchar(alpha);
 	}
-putchar(alpha);
 putchar('\n');
 return (0);
 }
