54ab1b0a562038a8fc4949fad60352288a4eb65c
ahmedarfaa <ahmedarfaa594@gmail.com>
2023-03-17 11:24:49 -0700

    5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int s;
+
+		for (s = 0 ; s <= 9 ; s++)
+			printf("%d", s);
+			printf("\n");
+
+
+	return (0);
+}
