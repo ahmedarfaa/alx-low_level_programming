fbc8ca286c69bcdd1aa8351792caba6e5b64241b
ahmedarfaa <ahmedarfaa594@gmail.com>
2023-03-21 06:09:45 -0700

    alpha

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char alpha;
+
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+	{	putchar(alpha);
+	}
+	putchar(\n);
+	return (0);
+}
