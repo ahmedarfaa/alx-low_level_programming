97e056be51aa7ab7cadb29e68e429dff7a52f3a2
ahmedarfaa <ahmedarfaa594@gmail.com>
2023-03-23 15:56:26 -0700

    4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -5,17 +5,19 @@
  *
  * Return: 0-9 followed by newline
  */
-void print_numbers(void)
+void print_most_numbers(void)
 {
 	int i;
 
 	for (i = 0; i <= 9; i++)
 	{
-	
-		if ((i == 2 || (i == 4))
+
+		if ((i == 2) || (i == 4))
 			continue;
 		else
 			 _putchar(i + '0');
+	
 	}
+
 		_putchar('\n');
 }
