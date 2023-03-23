957f7618fb1e21854fc225d45f7ac26626242d50
ahmedarfaa <ahmedarfaa594@gmail.com>
2023-03-23 15:02:25 -0700

    2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * mul - E
+ * @a: default
+ * @b: default
+ * Return: 0.
+ */
+
+int mul(int a, int b)
+{
+	int sum;
+
+	sum = a + b;
+	 return (sum);
+
+
+	return (0);
+}
