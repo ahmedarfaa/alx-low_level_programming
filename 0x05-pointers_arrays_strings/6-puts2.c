2e69da5ae9e71561f4f997e6ffc5ac8fa0e37892
ahmedarfaa <ahmedarfaa594@gmail.com>
2023-03-27 12:50:30 -0700

    6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -12,12 +12,13 @@ void puts2(char *str)
 
 
 	while (str[j] != '\0')
-	{	j++
+	{	j++;
 	}
 
-	for (i = 0; i < j; i = +2)
+	for (i = 0; i < j; i += 2)
 
-	{	_putchar(str[i]);
+	{
+		_putchar(str[i]);
 	}
 	_putchar('\n');
 
