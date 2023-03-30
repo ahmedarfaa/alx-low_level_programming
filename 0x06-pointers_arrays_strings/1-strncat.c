f0da981e750e8539deefd51a6001452d1121773f
ahmedarfaa <ahmedarfaa594@gmail.com>
2023-03-30 06:19:21 -0700

    first task strncat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+/**
+ * *_strncat - EP
+ * @dest: p
+ * @src: p
+ * @n: V
+ * Return: dest.
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	strcat(dest, src);
+	return (dest);
+}
