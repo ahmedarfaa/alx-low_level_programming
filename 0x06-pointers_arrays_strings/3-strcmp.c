532dc9f2af43239c9ab94d5a9b9b7ad472bc1f3b
ahmedarfaa <ahmedarfaa594@gmail.com>
2023-03-30 07:06:22 -0700

    the 4'th task && proto added to main.h

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,6 +9,15 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	strcmp(s1, s2);
-	return (dest);
+	int i;
+
+	for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
+	{
+		if (s1[i] != s2[i])
+		{
+			return (s1[i] - s2[i]);
+		}
+	}
+	return (0);
+
 }
