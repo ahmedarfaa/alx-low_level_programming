dff2e15202d3010a113e0f7f54ce927b51fc615b
ahmedarfaa <ahmedarfaa594@gmail.com>
2023-03-30 06:38:18 -0700

    the 4'th task

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+/**
+ * *_strcmp - EP
+ * @s1: p
+ * @s2: p
+ * Return: dest.
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	strcmp(s1, s2);
+	return (dest);
+}
