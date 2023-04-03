d45e0db0028f6f9931b1e87238683d98fdbb3cbc
ahmedarfaa <ahmedarfaa594@gmail.com>
2023-04-03 07:27:05 -0700

    first task

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _memset - Entery
+ * @s: pointer
+ * @b: string
+ * @n: number of bytes
+ * Return: s.
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	memset(s, b, n);
+
+
+}
