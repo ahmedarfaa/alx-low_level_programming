f6989d007863645e1c5630c7a7e93205ed1fb212
ahmedarfaa <ahmedarfaa594@gmail.com>
2023-04-05 08:46:11 -0700

    5'th task

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - E
+ * @x: var
+ * @y: var
+ * Return: 0.
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	if (y == 1)
+		return (x);
+	else
+		return (x * factorial(x, y - 1));
+}
