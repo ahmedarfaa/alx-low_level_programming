32354fa03f61b689a8e7f5caeca255bc058ef8cc
ahmedarfaa <ahmedarfaa594@gmail.com>
2023-04-08 08:12:21 -0700

    main directory with readme and first task

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - e
+ * @argc: v
+ * @argv: v
+ * Return: 0.
+ */
+
+int main(int argc, char const *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
