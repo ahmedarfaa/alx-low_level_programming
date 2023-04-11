d2d44af02e79ee0d69c67a924a1c26f003c709c7
ahmedarfaa <ahmedarfaa594@gmail.com>
2023-04-11 11:47:30 -0700

    edit task 1

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -9,13 +9,13 @@
 
 char *_strdup(char *str)
 {
-	int i, j;
+	int i = 0, j = 0;
 	char *ptr;
 
 	if (str == 0)
 		return (NULL);
 
-	for (i = 0; str[i] != '\0'; i++)
+	for (; str[i] != '\0'; i++)
 		ptr = malloc(i * sizeof(*str) + 1);
 
 	if (ptr == 0)
@@ -24,7 +24,7 @@ char *_strdup(char *str)
 	}
 	else
 	{
-		for (j = 0; j < i; j++)
+		for (; j < i; j++)
 			ptr[j] = str[j];
 	}
 	return (ptr);
