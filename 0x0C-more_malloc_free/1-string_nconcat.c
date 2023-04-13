2e486b860448a3381a277c69bb49dfa464ee019f
ahmedarfaa <ahmedarfaa594@gmail.com>
2023-04-13 13:34:55 -0700

    edit task 2,3

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -10,23 +10,41 @@
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int l1 = strlen(s1);
-	unsigned int l2 = strlen(s2);
+	unsigned int l = 0, m = 0, j = 0, i = 0;
 	char *ptr;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	if (n >= l2)
-		n = l2;
-	ptr = malloc(l1 + n + 1);
+	while (s1[l])
+		l++;
+	while (s2[m])
+		m++;
+
+	if (n >= i)
+		i = l + m;
+	else
+		i = l + n;
+	ptr = malloc(sizeof(char) * l + 1);
 
 	if (ptr == NULL)
 		return (NULL);
+	m = 0;
+	while (j < i)
+	{
+		if (j <= l)
+			ptr[j] = s2[j];
+		if (j >= l)
+		{
+			ptr[j] = s2[m];
+			m++;
+		}
+		j++;
 
-	strcpy(ptr, s1);
-	strncat(ptr, s2, n);
+	}
+	ptr[j] = '\0';
+
 	return (ptr);
 
 }
--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -9,13 +9,25 @@
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	void *ptr = malloc(nmemb * size);
+	int i = 0, l = 0;
+	int *ptr;
 
+	l = nmemb * size;
+
+	ptr = malloc(l);
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 	if (ptr == NULL)
 		return (NULL);
 
+
+	l = 0;
+	while (i < l)
+	{
+
+	ptr[i] = 0;
+	i++;
+	}
 	return (ptr);
 
 }
