df09ed1757dd8ab6e7309ff275fae4a9b72e1e56
ahmedarfaa <124623373+ahmedarfaa@users.noreply.github.com>
2023-04-17 19:22:19 +0200

    Update 2-print_dog.c
    Git hub commit

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -12,7 +12,7 @@ void print_dog(struct dog *d)
 	if (d != NULL)
 	{
 		printf("Name: %s\n", (d->name) ? d->name : "(nil)");
-		printf("Age: %.6f\n", (d->age));
+		printf("Age: %.6f\n", (d->age) ? d->age : 0);
 		printf("owner: %s\n", (d->owner) ? d->owner : "(nil)");
 	}
 
