9eee5c2615244a386dfe4f73fd145fa779c29d36
ahmedarfaa <ahmedarfaa594@gmail.com>
2023-04-19 14:14:07 -0700

    task array iteratot cerlibrackets misssed

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -13,11 +13,12 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int x = 0;
 
-	if (array != NULL && size > 0 && actio != NULL)
+	if (array != NULL && size > 0 && action != NULL)
 	{
 		while (x < size)
 		{
 			action(array[x]);
 			x++;
 		}
+	}
 }
