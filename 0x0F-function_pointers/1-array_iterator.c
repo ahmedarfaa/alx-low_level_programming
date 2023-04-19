447f744dd2852fec9aba0ed1475acba2fe21fdd2
ahmedarfaa <ahmedarfaa594@gmail.com>
2023-04-19 14:11:29 -0700

    task array iteratot

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,23 @@
+#include "function_pointers.h"
+#include <stddef.h>
+
+/**
+ * array_iterator - E
+ * @size: ..
+ * @array: ..
+ * @action: ptr
+ * Return: void.
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int x = 0;
+
+	if (array != NULL && size > 0 && actio != NULL)
+	{
+		while (x < size)
+		{
+			action(array[x]);
+			x++;
+		}
+}
