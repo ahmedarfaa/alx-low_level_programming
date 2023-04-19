0f285106a39b041ccc2820e58205e47fa0e44405
ahmedarfaa <ahmedarfaa594@gmail.com>
2023-04-19 13:45:49 -0700

    headers

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,5 +1,6 @@
 #ifndef _FUNCTION_POINTERS_H_
 #define _FUNCTION_POINTERS_H_
+#include <stdio.h>
 
 int int_index(int *array, int size, int (*cmp)(int));
 void array_iterator(int *array, size_t size, void (*action)(int));
