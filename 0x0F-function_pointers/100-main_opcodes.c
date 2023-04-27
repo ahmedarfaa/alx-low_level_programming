4a7f344b4d108082f3d6005aeecb3450a8e459c5
ahmedarfaa <ahmedarfaa594@gmail.com>
2023-04-27 10:31:06 -0700

    advanced task 's' missing

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,4 +1,4 @@
-#include "function_pointer.h"
+#include "function_pointers.h"
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -17,11 +17,11 @@ int main(int argc, char **argv)
 
 	if (argc != 2)
 		printf("Error\n"), exit(1);
-	i = atoi(argv[1];
+	i = atoi(argv[1]);
 
 			if (i < 0)
 			printf("Error\n"), exit(2);
 	while (i--)
-	printf("%02hhx%s", *s++, b ? " " : "\n");
+	printf("%02hhx%s", *s++, i ? " " : "\n");
 	return (0);
 }
