aece979474b97f2e68f7f523069d1cfcf0ddb09d
ahmedarfaa <ahmedarfaa594@gmail.com>
2023-04-27 10:28:12 -0700

    advanced task

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,27 @@
+#include "function_pointer.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - E
+ *
+ * @argc: ..
+ * @argv: ..
+ * Return: 0.
+ */
+
+int main(int argc, char **argv)
+{
+	char *s = (char *)main;
+	int i;
+
+	if (argc != 2)
+		printf("Error\n"), exit(1);
+	i = atoi(argv[1];
+
+			if (i < 0)
+			printf("Error\n"), exit(2);
+	while (i--)
+	printf("%02hhx%s", *s++, b ? " " : "\n");
+	return (0);
+}
