fe05732005f810fd61d715de0733647aa6a41e91
ahmedarfaa <ahmedarfaa594@gmail.com>
2023-04-27 08:55:04 -0700

    added directory linked list and the first task

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,40 @@
+#include "list.h"
+
+/**
+ * _len - E
+ *
+ * @s: string
+ * Return: length.
+ */
+
+int _len(char *s)
+{
+	int i = 0;
+
+	if (!s)
+		return (0);
+
+	while (*s++)
+		i++;
+	return (i);
+}
+
+
+/**
+ * print_list - E
+ * @h: pointer to struct
+ * Return: size.
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t n = 0;
+
+	while (h)
+	{
+		printf("[%d] %s\n", _len(h->str), h->str ? h->str : "(nil)");
+		h = h->next;
+		n++;
+	}
+	return (n);
+}
