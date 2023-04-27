6c15d4ee8bea18fca89bc1129b13d72871ef443c
ahmedarfaa <ahmedarfaa594@gmail.com>
2023-04-27 10:14:16 -0700

    advanced tasks

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -21,6 +21,8 @@ typedef struct list_s
 	struct list_s *next;
 } list_t;
 
+
+void _constructor(void) __attribute__ ((constructor));
 int _len(char *s);
 void free_list(list_t *head);
 size_t print_list(const list_t *h);
