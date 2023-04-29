00d1ff46da2fddbd5faa4f26199a500640ce3088
ahmedarfaa <ahmedarfaa594@gmail.com>
2023-04-29 12:30:59 -0700

    edit task 10 if gaurds {}

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -10,8 +10,8 @@
 
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-	listint_t *ptr = *head;
-	listint_t *prev = NULL;
+	listint_t *ptr;
+	listint_t *prev;
 	unsigned int i;
 
 	if (head == NULL || *head == NULL)
@@ -24,11 +24,13 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		return (1);
 	}
 
+	ptr = *head;
 	for (i = 0; i < index - 1; i++)
 	{
 	if (ptr->next == NULL)
+	{
 		return (-1);
-
+	}
 		ptr = ptr->next;
 	}
 	prev = ptr->next;
