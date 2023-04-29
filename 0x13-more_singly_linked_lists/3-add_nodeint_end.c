044127dab683d833f7ccd02ec355af98c65d0c70
ahmedarfaa <ahmedarfaa594@gmail.com>
2023-04-29 10:53:14 -0700

    4'th task

---
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+
+/**
+ * add_nodeint_end - E
+ * @head: ..
+ * @n: ..
+ *
+ * Return: ..
+ */
+
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+	listint_t *ptr1 = *head;
+	listint_t *ptr2 = malloc(sizeof(listint_t));
+
+	ptr2->n = n;
+	ptr2->next = NULL;
+
+	if (*head == NULL)
+	{
+	*head = ptr2;
+	}
+	else
+	{
+	while (ptr1->next != NULL)
+	{
+	ptr1 = ptr1->next;
+	}
+	ptr1->next = ptr2;
+	}
+	return (ptr2);
+}
