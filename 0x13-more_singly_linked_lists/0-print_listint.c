3c8eeefebf76f81d617dbc8fa645a1d8798a72b9
ahmedarfaa <ahmedarfaa594@gmail.com>
2023-04-29 10:32:22 -0700

    task more linked lists added directory and readme and firft task

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * print_listint - E
+ * @h: head of likedlist
+ *
+ * Return: count.
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	const listint_t *p = h;
+	int count = 0;
+	
+    while (p != NULL)
+	{
+	printf("%d\n", p->n);
+	count++;
+	p = p->next;
+	}
+	return (count);
+
+
+}
