18ec7413b6c058f060cdac9deb0bc39977ec4d69
ahmedarfaa <ahmedarfaa594@gmail.com>
2023-05-04 08:49:56 -0700

    task no 4

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * set_bit - E
+ * @n: ..
+ * @index: ..
+ * Return: ..
+ */
+
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	if (index > 63)
+		return (-1);
+
+	*n = ((1UL << index) | *n);
+	return (1);
+}
