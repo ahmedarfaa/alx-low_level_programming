63c915bfdbc067af09f1bfb7e6d234bd20ceb1b4
ahmedarfaa <ahmedarfaa594@gmail.com>
2023-05-04 08:43:11 -0700

    task no 2 ' missing alot missing

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -7,14 +7,14 @@
  */
 void print_binary(unsigned long int n)
 {
-	int x;
-	unsigned long int count = 0;
+	int x, count = 0;
+	unsigned long int current;
 
 	for (x = 63; x >= 0; x--)
 	{
-		count = n >> x;
+		current = n >> x;
 
-		if (count & x)
+		if (current & 1)
 		{
 			_putchar('1');
 			count++;
