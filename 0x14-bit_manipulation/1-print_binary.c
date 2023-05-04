81b8b2c6d19df5eb7477032402add660d97e2382
ahmedarfaa <ahmedarfaa594@gmail.com>
2023-05-04 08:35:27 -0700

    task no 2

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_binary - E
+ * @n: ..
+ * Return: ..
+ */
+void print_binary(unsigned long int n)
+{
+	int x;
+	unsigned long int count = 0;
+
+	for (x = 63; x >= 0; x--)
+	{
+		count = n >> i;
+
+		if (count & i)
+		{
+			_putchar('1);
+			count++;
+		}
+		else if (count)
+			_putchar('0');
+	}
+	if (!count)
+		_putchar('0');
+}
