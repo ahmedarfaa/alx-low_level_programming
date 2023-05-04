719db1778aab35ecf930e0fc32b02bc31522b97b
ahmedarfaa <ahmedarfaa594@gmail.com>
2023-05-04 08:23:27 -0700

    bitwise dir && readmefile && first task

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - E
+ * @b: ..
+ * Return: ..
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	int i;
+	unsigned int count = 0;
+
+	if (!b)
+		return (0);
+	for (i = 0; b[i]; i++)
+	{
+		if (b[i] < '0' || b[i] > '1')
+			return (0);
+		count = 2 * count + (b[i] - '0');
+	}
+	return (count);
+}
